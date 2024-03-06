eb3665e61241c6d5f4e52c0ad1889cfb666deb9f
elizzrss <korolkova.elizaveta4@gmail.com>
2024-03-06 17:39:22 +0500

    начало шапки

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,30 @@ import MyCode from './components/MyCode.vue'
 </script>
 
 <template>
-  <h1 class="w-2/4">Hello</h1>
+	<div class="bg-white w-4/5 h-screen m-auto mt-14 rounded-xl shadow-2xl">
+		<header>
+			<div>
+				<img src="/logo.png" alt="Logo">
+				<div>
+					<h2>Название магазина</h2>
+					<p>Онлайн-магазин кроссовок</p>
+				</div>
+			</div>
+
+			<ul>
+				<li>
+					<img src="/cart.svg" alt="Cart">
+					<b>0 руб.</b>
+				</li>
+				<li>
+					<img src="/heart.svg" alt="Heart">
+					<b>Избранное</b>
+				</li>
+				<li>
+					<img src="/profile.svg" alt="Profile">
+					<b>Профиль</b>
+				</li>
+			</ul>
+		</header>
+	</div>
 </template>
