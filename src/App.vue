b486159045c2809b2d292f91223b1fc3be506242
evaBro100 <kharchenko.eva@gmail.com>
2024-03-08 18:54:52 +0500

    верстка корзины и поиска с сортировкой

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,33 @@
 <script setup>
 import Header from './components/Header.vue';
+import Drawer from './components/Drawer.vue'
 import CardList from './components/CardList.vue';
 </script>
 
 <template>
+	<!-- <Drawer/> -->
 	<div class="bg-white w-4/5 m-auto mt-14 rounded-xl shadow-2xl">
 		<Header />
 		<div class="p-10">
-			<h2 class="text-3xl font-bold mb-10">Меню</h2>
+			<div class="flex justify-between items-center">
+				<h2 class="text-3xl font-bold mb-10">Меню</h2>
 
+				<div class="flex gap-4">
+					<select class="py-2 px-3 border rounded-md outline-none">
+					<option>По наименованию</option>
+					<option>По цене (дешевле)</option>
+					<option>По цене (дороже)</option>
+				</select>
+				<div class="relative">
+					<img class="absolute left-4 top-3"
+					src="/search.svg">
+
+					<input class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+					type="text"
+					placeholder="Поиск...">
+				</div>
+				</div>
+			</div>
 			<CardList/>
 		</div>
 	</div>
---
+++ src/components/CartItem.vue
@@ -0,0 +1,14 @@
+<template>
+
+    <div class="flex items-center border border-slate-200 p-4 rounded-xl gap-4">
+		<img class="w-16 h-10" src="/coffe/cappuccino.png" alt="Cappuccino">
+
+        <div class="flex flex-col">
+            <p>Каппучино S с корицей</p>
+            <div class="flex justify-between mt-2">
+                <b>269 руб.</b>
+                <img class="opacity-40 hover:opacity-100 cursor-pointer transition" src="/close.svg">
+            </div>
+        </div>
+    </div>
+</template>
