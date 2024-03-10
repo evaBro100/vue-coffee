8c13ca69798966c6c9f194d9ac225e3d50ed3327
elizzrss <korolkova.elizaveta4@gmail.com>
2024-03-10 16:32:13 +0500

    card

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -4,12 +4,76 @@ import Card from './Card.vue'
 
 <template>
   <div class="grid grid-cols-4 gap-5">
-    <Card title="Латте" imageUrl="/coffe/latte.png" :price="250" />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+	<Card 
+	  title="Эспрессо" 
+	  imageUrl="/coffe/espresso.png" 
+	  :price="200" 
+	  :isFavorite="false"
+	/>
+	<Card 
+	  title="Американо" 
+	  imageUrl="/coffe/americano.png" 
+	  :price="250" 
+	  :isFavorite="false"
+	/>
+    <Card 
+	  title="Капучино" 
+	  imageUrl="/coffe/cappuccino.png" 
+	  :price="250" 
+	  :isFavorite="false"
+	/>
+    <Card 
+	  title="Капучино с карамелью" 
+	  imageUrl="/coffe/cappuccinoCaramel.png" 
+	  :price="300"
+	/>
+    <Card 
+	  title="Латте" 
+	  imageUrl="/coffe/latte.png" 
+	  :price="250" 
+	  :isFavorite="false"
+	/>
+	<Card 
+	  title="Латте с карамелью" 
+	  imageUrl="/coffe/latteCaramel.png" 
+	  :price="250" 
+	  :isFavorite="false"
+	/>
+	<Card 
+	  title="Латте с шоколадом" 
+	  imageUrl="/coffe/latteChocolate.png" 
+	  :price="300" 
+	  :isFavorite="false"
+	/>
+	<Card 
+	  title="Флэт Уайт" 
+	  imageUrl="/coffe/flat.png" 
+	  :price="300" 
+	  :isFavorite="false"
+	/>
+	<Card 
+	  title="Черный чай" 
+	  imageUrl="/coffe/tea.png" 
+	  :price="200" 
+	  :isFavorite="false"
+	/>
+	<Card 
+	  title="Зеленый чай" 
+	  imageUrl="/coffe/teagreen.png" 
+	  :price="200" 
+	  :isFavorite="false"
+	/>
+	<Card 
+	  title="Чай с грейпфрутом" 
+	  imageUrl="/coffe/teaGrapefruit.png" 
+	  :price="250" 
+	  :isFavorite="false"
+	/>
+	<Card 
+	  title="Чай с апельсином" 
+	  imageUrl="/coffe/teaOrange.png" 
+	  :price="250" 
+	  :isFavorite="false"
+	/>
   </div>
 </template>
