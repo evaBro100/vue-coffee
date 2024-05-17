1b22f3c791c436094a07fe50edb7141ecc9cf223
elizzrss <korolkova.elizaveta4@gmail.com>
2024-05-17 21:57:09 +0500

    Функционал корзины готов

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,19 @@
+<script setup>
+defineProps({
+  totalPrice: Number
+})
+const emit = defineEmits(['openDrawer'])
+
+import axios from 'axios'
+import { telegramLoginTemp } from 'vue3-telegram-login'
+
+const onLogin = async (user) => {
+  const res = await axios.post(`${import.meta.env.VITE_HOST_API}auth/telegram`, user)
+  // TODO надо куда-то в стор
+  console.log(res)
+}
+</script>
+
 <template>
   <header class="flex justify-between border-b border-slate-300 px-8">
     <div class="flex items-center gap-4">
@@ -10,9 +26,12 @@
 
     <!-- TODO скрывать кнопки до входа в акк и придумать кнопку выхода -->
     <ul class="flex items-center gap-10">
-      <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
+      <li
+        @click="emit('openDrawer')"
+        class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black"
+      >
         <img src="/cart.svg" alt="Cart" />
-        <b>0 руб.</b>
+        <b>{{ totalPrice }} руб.</b>
       </li>
       <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
         <img src="/heart.svg" alt="Heart" />
@@ -26,16 +45,5 @@
     </ul>
   </header>
 </template>
-
-<script setup>
-import axios from 'axios'
-import { telegramLoginTemp } from 'vue3-telegram-login'
-
-const onLogin = async (user) => {
-  const res = await axios.post(`${import.meta.env.VITE_HOST_API}auth/telegram`, user)
-  // TODO надо куда-то в стор
-  console.log(res)
-}
-</script>
 
 <style lang="scss" scoped></style>
