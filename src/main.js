eda1847dea033e4d7e9582396415508d6ed490e0
evaBro100 <kharchenko.eva@gmail.com>
2024-05-26 21:34:14 +0500

    прикрутила анимацию auto-animate + в main.js import

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
@@ -9,3 +10,4 @@ const pinia = createPinia()
 const app = createApp(App)
 app.use(pinia)
 app.mount('#app')
+app.use(autoAnimatePlugin)
